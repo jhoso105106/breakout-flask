d15fe93fa4eb7f912b652b7601221e6ca2aa0b32
JUNKO HOSONO <hjun105106@hotmail.co.jp>
2025-05-05 23:42:05 +0900

    修正内容の説明（例：ゲームオーバー時の挙動を改善）

--- static/game.js
+++ static/game.js
@@ -123,15 +123,16 @@ function draw() {
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if(y + dy < ballRadius) {
-    dy = -dy;
-  } else if(y + dy > canvas.height-ballRadius) {
-    if(x > paddleX && x < paddleX + paddleWidth) {
+  if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
-    }
-    else {
+    } else {
       alert("ゲームオーバー");
-      document.location.reload();
+      isGameOver = true;
+      setTimeout(function() {
+        document.location.reload();
+      }, 100);
+      return;
     }
   }
 
